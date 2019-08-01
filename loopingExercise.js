ad13ad70683a5ad3b0c814fdba6c53f6715d3fe9
GustiPutraJarmana <gustimertaputra@gmail.com>
2019-08-01 22:53:57 +0700

    This is Looping exercise

---
+++ loopingExercise.js
@@ -0,0 +1,39 @@
+console.log("<======================================>");
+console.log("While Looping");
+// 1. While Looping
+var angka = 1;
+while (angka < 20) {
+  angka++;
+  console.log(angka + " - I love coding");
+}
+
+// OUTPUT
+
+// LOOPING PERTAMA
+// 2 - I love coding
+// 4 - I love coding
+// 6 - I love coding
+// 8 - I love coding
+// 10 - I love coding
+// 12 - I love coding
+// 14 - I love coding
+// 16 - I love coding
+// 18 - I love coding
+// 20 - I love coding
+console.log("<======================================>");
+var angka2 = 21;
+while (angka2 > 2) {
+  angka2--;
+  console.log(angka2 + " - I will become fullstack developer");
+}
+// LOOPING KEDUA
+// 20 - I will become fullstack developer
+// 18 - I will become fullstack developer
+// 16 - I will become fullstack developer
+// 14 - I will become fullstack developer
+// 12 - I will become fullstack developer
+// 10 - I will become fullstack developer
+// 8 - I will become fullstack developer
+// 6 - I will become fullstack developer
+// 4 - I will become fullstack developer
+// 2 - I will become fullstack developer
